74be794b444c1d77cda5423a4939307f6fc14d27
oktayafandi <ogtayaa@code.edu.az>
2021-09-16 01:47:52 +0400

    WearShop

--- wearproject/js/index.js
+++ wearproject/js/index.js
@@ -9,3 +9,16 @@ signUpButton.addEventListener("click", () => {
 signInButton.addEventListener("click", () => {
   container.classList.remove("right-panel-active");
 });
+
+function openLeftMenu() {
+  document.getElementById("leftMenu").style.display = "block";
+}
+
+function closeLeftMenu() {
+  document.getElementById("leftMenu").style.display = "none";
+}
+
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
